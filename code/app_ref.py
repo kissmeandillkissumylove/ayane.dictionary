ed2db9f93532c8751cfaa5b02b63cc970a663bf6
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-18 14:24:46 +0700

    add label; change several settings;

--- code/app_ref.py
+++ code/app_ref.py
@@ -1,6 +1,7 @@
 """it's an implementation of dictionary on cards by ayane miuro
 02/07/2024 https://github.com/kissmeandillkissumylove"""
 import tkinter
+from tkinter import NW
 
 from constants import *
 
@@ -26,9 +27,38 @@ class App(Singleton, tkinter.Tk):
 	def __init__(self, master=None):
 		"""method __init__ of class Singleton."""
 		super().__init__(master)
+		self._setup_while_startup()
+		self.items = {}
+
+	def _setup_while_startup(self):
+		"""setting settings when starting the application."""
 		self.title(TITLE)
+		try:
+			self.iconbitmap(ICON_PATH)
+		except tkinter.TclError:
+			# icon didn't load
+			pass
 		self.geometry(GEOMETRY)
 		self.resizable(False, False)
+		self.configure(
+			background=BLACK,
+			bd=0,
+		)
+		self._create_and_display_word_label()
+
+	def _create_and_display_word_label(self) -> None:
+		"""returns label that will act as a screen for displaying the word."""
+		tkinter.Label(
+			master=self.master,
+			background=GREY,
+			bd=0,
+			anchor=NW,
+			pady=0,
+			padx=2,
+			wraplength=286,
+			justify="left",
+			foreground=BLACK,
+		).place(x=5, y=5, width=490, height=50)
 
 
 def main():
--- code/constants.py
+++ code/constants.py
@@ -1,2 +1,8 @@
+# main window settings
 TITLE = "ᴋɪssᴍᴇᴀɴᴅɪʟʟᴋɪssᴜᴍʏʟöᴠᴇ"
 GEOMETRY = "500x400"
+ICON_PATH = r"..\data\icon.ico"
+
+# colors
+BLACK = "#000000"
+GREY = "#B5B5B5"
