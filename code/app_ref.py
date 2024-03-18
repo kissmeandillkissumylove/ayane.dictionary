bf4f25661916e6bbf75f4955f659e7b3d30941a6
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-18 15:56:22 +0700

    add labels; add buttons;

--- code/app_ref.py
+++ code/app_ref.py
@@ -1,7 +1,7 @@
 """it's an implementation of dictionary on cards by ayane miuro
 02/07/2024 https://github.com/kissmeandillkissumylove"""
 import tkinter
-from tkinter import NW
+from tkinter import NW, W
 
 from constants import *
 
@@ -28,37 +28,180 @@ class App(Singleton, tkinter.Tk):
 		"""method __init__ of class Singleton."""
 		super().__init__(master)
 		self._setup_while_startup()
-		self.items = {}
 
 	def _setup_while_startup(self):
 		"""setting settings when starting the application."""
+		self._main_window_setup()
+
+		self._word_label = self._create_word_label()
+		self._translation_label = self._create_translation_label()
+		self._next_button = self._create_next_button()
+		self._show_button = self._create_show_button()
+		self._forgot_button = self._create_forgot_button()
+		self._mistakes_text_label = self._create_mistakes_text_label()
+		self._mistakes_counter_label = self._create_mistakes_counter_label()
+		self._words_text_label = self._create_words_text_label()
+		self._words_counter_label = self._create_words_counter_label()
+
+		self._display_interface()
+
+	def _main_window_setup(self) -> None:
+		"""sets main window settings."""
 		self.title(TITLE)
-		try:
-			self.iconbitmap(ICON_PATH)
-		except tkinter.TclError:
-			# icon didn't load
-			pass
 		self.geometry(GEOMETRY)
 		self.resizable(False, False)
 		self.configure(
 			background=BLACK,
 			bd=0,
 		)
-		self._create_and_display_word_label()
+		try:
+			self.iconbitmap(ICON_PATH)
+		except tkinter.TclError:
+			pass  # icon didn't load
 
-	def _create_and_display_word_label(self) -> None:
-		"""returns label that will act as a screen for displaying the word."""
-		tkinter.Label(
+	def _create_word_label(self) -> tkinter.Label:
+		"""creates and displays a label that will act as a screen for displaying the
+			word."""
+		return tkinter.Label(
 			master=self.master,
 			background=GREY,
 			bd=0,
 			anchor=NW,
 			pady=0,
 			padx=2,
-			wraplength=286,
+			wraplength=486,
 			justify="left",
 			foreground=BLACK,
-		).place(x=5, y=5, width=490, height=50)
+		)
+
+	def _create_translation_label(self) -> tkinter.Label:
+		"""creates and displays a label that will act as a screen for displaying the
+			word."""
+		return tkinter.Label(
+			master=self.master,
+			background=GREY,
+			bd=0,
+			anchor=NW,
+			pady=0,
+			padx=2,
+			wraplength=486,
+			justify="left",
+			foreground=BLACK,
+		)
+
+	def _create_next_button(self) -> tkinter.Button:
+		"""creates and displays a "NEXT" button which will display a next word from
+			a dictionary."""
+		return tkinter.Button(
+			master=self.master,
+			background=RED,
+			bd=2,
+			foreground=WHITE,
+			activebackground=BLUE,
+			text=TEXT_NEXT_BUTTON,
+			command=self._next_button_command,
+			state="normal",
+		)
+
+	def _create_show_button(self) -> tkinter.Button:
+		"""creates and displays a 'NEXT' button that will display the translation of
+			the current word."""
+		return tkinter.Button(
+			master=self.master,
+			background=RED,
+			bd=2,
+			foreground=WHITE,
+			activebackground=BLUE,
+			text=TEXT_SHOW_BUTTON,
+			command=self._show_button_command,
+			state="disabled",
+		)
+
+	def _create_forgot_button(self) -> tkinter.Button:
+		"""creates and displays a "FORGOT" button which adds a mistake to the
+			mistakes counter."""
+		return tkinter.Button(
+			master=self.master,
+			background=RED,
+			bd=2,
+			foreground=WHITE,
+			activebackground=BLUE,
+			text=TEXT_FORGOT_BUTTON,
+			command=self._forgot_button_command,
+			state="disabled",
+		)
+
+	def _create_mistakes_text_label(self) -> tkinter.Label:
+		"""creates and displays a "mistakes" label just for text."""
+		return tkinter.Label(
+			master=self.master,
+			background=BLUE,
+			bd=0,
+			anchor=W,
+			foreground=WHITE,
+			text=TEXT_MISTAKES_COUNTER_LABEL,
+		)
+
+	def _create_mistakes_counter_label(self) -> tkinter.Label:
+		"""creates and displays a label which will count the number of incorrect
+			answers."""
+		return tkinter.Label(
+			master=self.master,
+			background=GREY,
+			bd=0,
+			anchor=W,
+			foreground=WHITE,
+			text=999999,
+			font=FONT + BOLD,
+		)
+
+	def _create_words_text_label(self) -> tkinter.Label:
+		"""creates and displays a "words" label just for text."""
+		return tkinter.Label(
+			master=self.master,
+			background=BLUE,
+			bd=0,
+			anchor=W,
+			foreground=WHITE,
+			text=TEXT_WORD_LABEL,
+		)
+
+	def _create_words_counter_label(self) -> tkinter.Label:
+		"""creates and displays a label which will count the number of words in a
+			dictionary."""
+		return tkinter.Label(
+			master=self.master,
+			background=GREY,
+			bd=0,
+			anchor=W,
+			foreground=WHITE,
+			text="999998/999999",
+			font=FONT + BOLD,
+		)
+
+	def _next_button_command(self) -> None:
+		"""."""
+		pass
+
+	def _show_button_command(self) -> None:
+		"""."""
+		pass
+
+	def _forgot_button_command(self) -> None:
+		"""."""
+		pass
+
+	def _display_interface(self) -> None:
+		"""shows all interface elements."""
+		self._word_label.place(x=5, y=5, width=490, height=50)
+		self._translation_label.place(x=5, y=60, width=490, height=50)
+		self._next_button.place(x=5, y=115, width=50, height=25)
+		self._show_button.place(x=60, y=115, width=50, height=25)
+		self._forgot_button.place(x=115, y=115, width=50, height=25)
+		self._mistakes_text_label.place(x=170, y=115, width=50, height=25)
+		self._mistakes_counter_label.place(x=220, y=115, width=60, height=25)
+		self._words_text_label.place(x=285, y=115, width=40, height=25)
+		self._words_counter_label.place(x=325, y=115, width=170, height=25)
 
 
 def main():
--- code/constants.py
+++ code/constants.py
@@ -3,6 +3,22 @@ TITLE = "ᴋɪssᴍᴇᴀɴᴅɪʟʟᴋɪssᴜᴍʏʟöᴠᴇ"
 GEOMETRY = "500x400"
 ICON_PATH = r"..\data\icon.ico"
 
+# buttons settings
+TEXT_NEXT_BUTTON = "ɴᴇxᴛ"
+TEXT_SHOW_BUTTON = "sʜᴏᴡ"
+TEXT_FORGOT_BUTTON = "ꜰᴏʀɢᴏᴛ"
+
+# label settings
+TEXT_MISTAKES_COUNTER_LABEL = "ᴍɪsᴛᴀᴋᴇs:"
+TEXT_WORD_LABEL = "ᴡᴏʀᴅs:"
+
+# font
+FONT = "verdana 8"
+BOLD = " bold"
+
 # colors
 BLACK = "#000000"
 GREY = "#B5B5B5"
+RED = "#DF1313"
+WHITE = "#FFFFFF"
+BLUE = "#2D81B2"
