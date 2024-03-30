11ca6a34d61ea24c03332c5e74c05cc1b677acfd
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-30 21:57:25 +0700

    collect classes by files according the meaning

--- app/main.py
+++ app/main.py
@@ -1,59 +1,8 @@
-import re
 import tkinter
 
 from modules.JSONReader import JSONReader
-from modules.config_validation_types import ValidationTypes
-from modules.paths import CONFIG_TEMPLATE, CONFIG_0
-
-
-class ConfigValidationTemplate:
-	"""
-	contains valid types and patterns for checking the configuration file.
-	"""
-
-	def __init__(self):
-		"""
-		contains valid types and patterns for checking the configuration file.
-		"""
-		self._template = {
-			"types": ValidationTypes.load_types(),
-			"patterns": JSONReader.read_json(CONFIG_TEMPLATE)
-		}
-
-	def get_types(self) -> dict:
-		"""
-		:return: types dictionary.
-		"""
-		return self._template["types"]
-
-	def get_patters(self) -> dict:
-		"""
-		:return: patterns dictionary.
-		"""
-		return self._template["patterns"]
-
-
-class ConfigValidator:
-	"""
-	checks the validity of the configuration file.
-	"""
-
-	@classmethod
-	def validate_config(cls, config: dict) -> bool:
-		"""
-		checks that the config matches the types and template.
-		:param config: config dictionary.
-		:return: boolean.
-		"""
-		config_template = ConfigValidationTemplate()
-		for key, value in config.items():
-			if key not in config_template.get_types():
-				return False
-			if type(value) not in config_template.get_types()[key]:
-				return False
-			if not re.search(config_template.get_patters()[key], value):
-				return False
-		return True
+from modules.config_validator import ConfigValidator
+from modules.paths import CONFIG_0
 
 
 class App(tkinter.Tk):
