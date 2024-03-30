37280a54ee2d27423034b40da427b052fd9d75c6
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-30 21:58:07 +0700

    collect classes by files according the meaning

---
+++ app/modules/config_validator.py
@@ -0,0 +1,73 @@
+import re
+from dataclasses import dataclass
+
+from app.modules.JSONReader import JSONReader
+from app.modules.paths import CONFIG_TEMPLATE
+
+
+@dataclass
+class ValidationTypes:
+	"""
+	contains valid configuration file types.
+	"""
+
+	@classmethod
+	def load_types(cls) -> dict:
+		"""
+		creates valid types for configuration file.
+		"""
+		keys = JSONReader.read_json(CONFIG_TEMPLATE).keys()
+		valid_types = {}
+		for key in keys:
+			valid_types[key] = [str, None]
+		return valid_types
+
+
+class ConfigValidationTemplate:
+	"""
+	contains valid types and patterns for checking the configuration file.
+	"""
+
+	def __init__(self):
+		"""
+		contains valid types and patterns for checking the configuration file.
+		"""
+		self._template = {
+			"types": ValidationTypes.load_types(),
+			"patterns": JSONReader.read_json(CONFIG_TEMPLATE)
+		}
+
+	def get_types(self) -> dict:
+		"""
+		:return: types dictionary.
+		"""
+		return self._template["types"]
+
+	def get_patters(self) -> dict:
+		"""
+		:return: patterns dictionary.
+		"""
+		return self._template["patterns"]
+
+
+class ConfigValidator:
+	"""
+	checks the validity of the configuration file.
+	"""
+
+	@classmethod
+	def validate_config(cls, config: dict) -> bool:
+		"""
+		checks that the config matches the types and template.
+		:param config: config dictionary.
+		:return: boolean.
+		"""
+		config_template = ConfigValidationTemplate()
+		for key, value in config.items():
+			if key not in config_template.get_types():
+				return False
+			if type(value) not in config_template.get_types()[key]:
+				return False
+			if not re.search(config_template.get_patters()[key], value):
+				return False
+		return True
