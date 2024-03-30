8e64a232aa933839331ea1bd708c7ea9f9e4e6a9
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-30 16:56:14 +0700

    main structure

--- app/main.py
+++ app/main.py
@@ -1,22 +1,59 @@
+import re
 import tkinter
-from dataclasses import dataclass
-from typing import Union
+
+from modules.JSONReader import JSONReader
+from modules.config_validation_types import ValidationTypes
+from modules.paths import CONFIG_TEMPLATE, CONFIG_0
 
 
-@dataclass
-class ValidationTypes:
-	geometry = Union[str, None]
-	background = Union[str, None]
-	title = Union[str, None]
-	icon = Union[str, None]
+class ConfigValidationTemplate:
+	"""
+	contains valid types and patterns for checking the configuration file.
+	"""
+
+	def __init__(self):
+		"""
+		contains valid types and patterns for checking the configuration file.
+		"""
+		self._template = {
+			"types": ValidationTypes.load_types(),
+			"patterns": JSONReader.read_json(CONFIG_TEMPLATE)
+		}
+
+	def get_types(self) -> dict:
+		"""
+		:return: types dictionary.
+		"""
+		return self._template["types"]
+
+	def get_patters(self) -> dict:
+		"""
+		:return: patterns dictionary.
+		"""
+		return self._template["patterns"]
 
 
-@dataclass
-class ValidationPatterns:
-	geometry: Union[str, None] = r"^(\\d+)x(\\d+)(?:\\+\\d+)?(?:\\+\\d+)?$"
-	background: Union[str, None] = r"^#[0-9a-fA-F]{6}$",
-	title: Union[str, None] = r"^.{0,32}$",
-	icon: Union[str, None] = None
+class ConfigValidator:
+	"""
+	checks the validity of the configuration file.
+	"""
+
+	@classmethod
+	def validate_config(cls, config: dict) -> bool:
+		"""
+		checks that the config matches the types and template.
+		:param config: config dictionary.
+		:return: boolean.
+		"""
+		config_template = ConfigValidationTemplate()
+		for key, value in config.items():
+			if key not in config_template.get_types():
+				return False
+			if type(value) not in config_template.get_types()[key]:
+				return False
+			if not re.search(config_template.get_patters()[key], value):
+				return False
+		return True
 
 
 class App(tkinter.Tk):
@@ -30,11 +67,32 @@ class App(tkinter.Tk):
 		:param config: a dictionary containing configuration options for the
 			application window. defaults to None. valid keys include:
 				- geometry (str): the window geometry (e.g., "800x600+100+200").
-				- background (str): background color (e.g., "#ffffff", "#2D81B2").
 				- title (str): the title of the application window.
 				- icon (str or pathlib.Path): path to the application icon.
+				- background (str): background color (e.g., "#ffffff", "#2D81B2").
 		"""
 		super().__init__()
+
+		if config and ConfigValidator.validate_config(config):
+			self._config_setup(config)
+
+		else:
+			config = JSONReader.read_json(CONFIG_0)
+			if config and ConfigValidator.validate_config(config):
+				self._config_setup(config)
+
+	def _config_setup(self, config: dict):
+		"""
+		set settings by configuration dictionary.
+		:param config: dictionary.
+		"""
+		self.geometry(config["geometry"])
+		self.title(config["title"])
+		try:
+			self.iconbitmap(config["icon"])
+		except tkinter.TclError:
+			pass
+		self.configure(background=config["background"])
 
 
 def main():
