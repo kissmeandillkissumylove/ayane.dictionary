536ec21ac8a4d61ed05e30c8c0d566f98fcc35f8
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-31 13:35:46 +0700

    fix paths bug

--- app/modules/paths.py
+++ app/modules/paths.py
@@ -3,5 +3,6 @@ contains paths to files.
 """
 import os.path
 
-CONFIG_TEMPLATE = os.path.join("modules", "config_validation_patterns.json")
-CONFIG_0 = os.path.join("modules", "config_0.json")
+CONFIG_TEMPLATE = os.path.join(
+	os.path.dirname(__file__), "config_validation_patterns.json")
+CONFIG_0 = os.path.join(os.path.dirname(__file__), "config_0.json")
