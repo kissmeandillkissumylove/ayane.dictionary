d1b91585df8504e99fe6f81a9e449d60cbbb0286
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-02 11:38:44 +0700

    remove paths.py; add config.py;

---
+++ app/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+# MODULES.
+CONFIG_0 = BASE_DIR / "modules/config_0.json"
--- app/modules/paths.py
+++
@@ -1,8 +0,0 @@
-"""
-contains paths to files.
-"""
-import os.path
-
-CONFIG_TEMPLATE = os.path.join(
-	os.path.dirname(__file__), "config_validation_patterns.json")
-CONFIG_0 = os.path.join(os.path.dirname(__file__), "config_0.json")
