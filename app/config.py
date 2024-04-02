74f707ef29cdcb18879a747c9f802921dcea5cf2
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-02 18:49:01 +0700

    update config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-# MODULES.
-CONFIG_0 = BASE_DIR / "modules/config_0.json"
-CONFIG_TEMPLATE = BASE_DIR / "modules/config_validation_patterns.json"
+# JSON FILES.
+CONNECT_JSON = "modules/files_json"
+CONFIG_0 = BASE_DIR / CONNECT_JSON / "config_0.json"
+CONFIG_PATTERNS = (
+		BASE_DIR / CONNECT_JSON / "config_validation_patterns.json")
