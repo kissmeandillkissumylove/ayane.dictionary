595a9d70f5680250ece1f23e961e1562c4e01c33
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-03 21:20:31 +0700

    add new files

--- app/config.py
+++ app/config.py
@@ -10,6 +10,8 @@ CONFIG_PATTERNS = (
 
 NOT_EMPTY_JSON = BASE_DIR / "tests/tests_files/not_empty.json"
 EMPTY_JSON = BASE_DIR / "tests/tests_files/empty.json"
+CORRECT_CONFIG = BASE_DIR / "tests/tests_files/correct_config.json"
+INCORRECT_CONFIG = BASE_DIR / "tests/tests_files/incorrect_config.json"
 
 # TXT FILES.
 NOT_EMPTY_TXT = BASE_DIR / "tests/tests_files/not_empty.txt"
