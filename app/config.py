80aa30f5c76beea681ba8ac5ede4302781320da5
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-04 15:26:41 +0700

    fix bug with config.py. all the paths should be str

--- app/config.py
+++ app/config.py
@@ -4,14 +4,15 @@ BASE_DIR = Path(__file__).parent
 
 # JSON FILES.
 CONNECT_JSON = "modules/files_json"
-CONFIG_0 = BASE_DIR / CONNECT_JSON / "config_0.json"
-CONFIG_PATTERNS = (
-		BASE_DIR / CONNECT_JSON / "config_validation_patterns.json")
+CONFIG_0 = str(BASE_DIR / CONNECT_JSON / "config_0.json")
+CONFIG_PATTERNS = str((
+		BASE_DIR / CONNECT_JSON / "config_validation_patterns.json"))
 
-NOT_EMPTY_JSON = BASE_DIR / "tests/tests_files/not_empty.json"
-EMPTY_JSON = BASE_DIR / "tests/tests_files/empty.json"
-CORRECT_CONFIG = BASE_DIR / "tests/tests_files/correct_config.json"
-INCORRECT_CONFIG = BASE_DIR / "tests/tests_files/incorrect_config.json"
+NOT_EMPTY_JSON = str(BASE_DIR / "tests/tests_files/not_empty.json")
+EMPTY_JSON = str(BASE_DIR / "tests/tests_files/empty.json")
+CORRECT_CONFIG = str(BASE_DIR / "tests/tests_files/correct_config.json")
+INCORRECT_CONFIG = str(
+	BASE_DIR / "tests/tests_files/incorrect_config.json")
 
 # TXT FILES.
-NOT_EMPTY_TXT = BASE_DIR / "tests/tests_files/not_empty.txt"
+NOT_EMPTY_TXT = str(BASE_DIR / "tests/tests_files/not_empty.txt")
