9efd8cee9a05c4876aba6d864d2df7ecad6a96dc
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-06 20:40:13 +0700

    remove everything for a while

--- app/modules/custom_exceptions.py
+++
@@ -1,8 +0,0 @@
-class ConfigNotFoundError(Exception):
-	"""
-	configuration file not found.
-	"""
-
-	def __init__(self, message="configuration file not found"):
-		self.message = message
-		super().__init__(self.message)
