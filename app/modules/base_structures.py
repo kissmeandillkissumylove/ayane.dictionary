6f048cf571eb0629c25e5333f59e0aea19026045
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-06 21:53:35 +0700

    add BaseFileReader

--- app/modules/base_structures.py
+++ app/modules/base_structures.py
@@ -1,6 +1,6 @@
 """all the basic structures are collected here."""
-import abc
 import tkinter
+from abc import abstractmethod, ABC
 
 import injector
 from injector import Binder
@@ -9,21 +9,30 @@ from injector import Binder
 class BaseInjectionContainer(injector.Module):
 	"""abstract class to represent an injection container."""
 
-	@abc.abstractmethod
+	@abstractmethod
 	def configure(self, binder: Binder) -> None:
 		"""contains injections and the rules by which injections are
 		introduced."""
 		raise NotImplementedError
 
 
-class BaseWindow(abc.ABC, tkinter.Tk):
+class BaseWindow(ABC, tkinter.Tk):
 	"""abstract class to represent a window."""
 
 	def __init__(self):
 		"""BaseWindow __init__."""
 		super().__init__()
 
-	@abc.abstractmethod
+	@abstractmethod
 	def _setup_configuration(self, *args, **kwargs):
 		"""sets window configuration."""
+		raise NotImplementedError
+
+
+class BaseFileReader(ABC):
+	"""abstract class for reading a file."""
+
+	@abstractmethod
+	def read_(self, *args, **kwargs):
+		"""reads the file."""
 		raise NotImplementedError
