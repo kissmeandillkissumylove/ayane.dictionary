ca259016ae29f83d1a58b86adb6eef3518bf599d
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-08 13:01:06 +0700

    add a logger to main

--- app/main.py
+++ app/main.py
@@ -5,12 +5,16 @@ from injector import Injector
 from app.config import configuration_default
 from app.modules.file_readers import JsonFileReader
 from app.modules.injections_container import MainInjectionContainer
+from app.modules.logger import CustomLogger
 from app.modules.root_window import RootWindow
 
 
 def main():
 	"""main function which runs the application."""
 	_injection_container = Injector([MainInjectionContainer])
+	_logger_ = _injection_container.get(CustomLogger)
+	_logger_.set_logger_level(10)
+	_logger_.log(50, "start new logging cycle.")
 
 	root_window = _injection_container.get(RootWindow)
 	root_window.injection_container = _injection_container
@@ -20,6 +24,7 @@ def main():
 	root_window.configuration_dict = current_config
 
 	root_window.run()
+	_logger_.log(50, "termination of the program. stop logging.\n\n")
 
 
 if __name__ == "__main__":
