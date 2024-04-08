e86d8fc548534f01e8e39da013bf07de093ba1bf
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-08 12:32:52 +0700

    add path to the logging file

--- app/config.py
+++ app/config.py
@@ -7,3 +7,9 @@ connect_json = "modules/json"
 
 configuration_default = str(
 	BASE_DIR / connect_json / "configuration_default.json")
+
+# txt files.
+connect_txt = "modules/txt"
+
+logs = str(
+	BASE_DIR / connect_txt / "logs.log")
