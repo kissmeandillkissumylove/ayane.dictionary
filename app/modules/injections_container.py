31937e1a99e0a2aa9710abd15556d1faee5b90a5
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-08 13:00:15 +0700

    add a logger to the container

--- app/modules/injections_container.py
+++ app/modules/injections_container.py
@@ -1,13 +1,19 @@
 """contains injection containers."""
-from injector import Binder
+from injector import Binder, provider
 
 from app.modules.base_structures import BaseInjectionContainer
+from app.modules.logger import CustomLogger
 
 
 class MainInjectionContainer(BaseInjectionContainer):
 	"""main injection container."""
 
+	@provider
+	def provide_custom_logger(self) -> CustomLogger:
+		"""returns CustomLogger object."""
+		return CustomLogger(__name__)
+
 	def configure(self, binder: Binder) -> None:
 		"""contains injections and the rules by which injections are
 		introduced."""
-		...
+		binder.bind(CustomLogger, to=self.provide_custom_logger)
