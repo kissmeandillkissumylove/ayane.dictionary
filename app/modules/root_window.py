2ee4176fc2773430c35471c230e2ff425b29c48a
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-04-14 13:20:10 +0700

    rename _configuration

--- app/modules/root_window.py
+++ app/modules/root_window.py
@@ -14,63 +14,66 @@ class RootWindow(BaseWindow):
 		"""RootWindow __init__."""
 		super().__init__()
 
-		if isinstance(logger, CustomLogger):
-			self._logger = logger
-		else:
+		if not isinstance(logger, CustomLogger):
 			raise TypeError("wrong type for the logger. %s" % type(logger))
+		self._logger = logger
+		self._logger.log_debug("set: _logger", self._logger)
 
+		self._configuration_container = None
 		self._injection_container = None
-		self._configuration = None
 
 	def _setup_configuration(self, *args, **kwargs):
 		"""sets root window configuration."""
-		if self._configuration is not None:
-			self._logger.log_debug("start _setup_configuration()")
-
-			self.geometry(self._configuration["geometry"])
+		if self._configuration_container is not None:
+			root_config = self._configuration_container.root_window
 			self._logger.log_debug(
-				"set: geometry", self._configuration["geometry"])
+				"start _setup_configuration()", root_config)
+
+			self.geometry(root_config["geometry"])
+			self._logger.log_debug(
+				"set: geometry", root_config["geometry"])
 
 			self.resizable(False, False)
 			self._logger.log_debug("set: resizable(False, False)")
 
-			self.title(self._configuration["title"])
-			self._logger.log_debug("set: title", self._configuration["title"])
+			self.title(root_config["title"])
+			self._logger.log_debug("set: title", root_config["title"])
 
-			self.iconbitmap(self._configuration["icon"])
+			self.iconbitmap(root_config["icon"])
 			self._logger.log_debug(
-				"set: iconbitmap", self._configuration["icon"])
+				"set: iconbitmap", root_config["icon"])
 
-			self.configure(background=self._configuration["background"])
+			self.configure(background=root_config["background"])
 			self._logger.log_debug(
-				"set: background", self._configuration["background"])
+				"set: background", root_config["background"])
 
 	@property
-	def configuration(self) -> dict:
-		"""get _configuration value.
-		:return: dict object."""
-		return self._configuration
+	def configuration_container(self) -> ConfigContainer:
+		"""get _configuration_container value.
+		:return: ConfigContainer object."""
+		return self._configuration_container
 
-	@configuration.setter
-	def configuration(self, config: dict):
-		"""sets new value for _configuration.
-		:param config: dict object."""
-		if isinstance(config, dict):
-			self._configuration = config
+	@configuration_container.setter
+	def configuration_container(self, container: ConfigContainer):
+		"""sets new value for _configuration_container.
+		:param config: ConfigContainer object."""
+		if isinstance(container, ConfigContainer):
+			self._configuration_container = container
 			self._logger.log_debug(
 				"set: _configuration",
-				self._configuration)
+				self._configuration_container)
 		else:
-			self._logger.log_warning("try: _configuration", config)
+			self._logger.log_warning(
+				"try: _configuration_container", container)
 
-	@configuration.deleter
-	def configuration(self):
-		"""removes the reference to _configuration. subsequently this
-		object will be deleted."""
+	@configuration_container.deleter
+	def configuration_container(self):
+		"""removes the reference to _configuration_container.
+		subsequently this object will be deleted."""
 		self._logger.log_debug(
 			"del: _configuration",
-			self._configuration)
-		del self._configuration
+			self._configuration_container)
+		del self._configuration_container
 
 	def run(self):
 		"""launches a window."""
